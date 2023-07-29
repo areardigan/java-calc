705f8396cd6314e7e99f3b0cb4f06e1e0a9149ac
andrewreardigan <drewreardigan@gmail.com>
2023-07-29 15:40:22 -0400

    simple hour * hourly rate

--- src/main/java/basics/GrossPayCalculator.java
+++ src/main/java/basics/GrossPayCalculator.java
@@ -1,9 +1,28 @@
 package basics;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
 
     public static void main (String[] args) {
-        System.out.println("Hello!");
+        System.out.println("How many hours did you work?");
+        Scanner scanner = new Scanner(System.in);
+        int hours = scanner.nextInt();
+
+        System.out.println("What is your pay rate per hour?");
+        double rate = scanner.nextDouble();
+
+        scanner.close();
+
+        double payRate = hours * rate;
+
+        System.out.println("Gross pay is: " + payRate);
+
+
+
+
+
+
 
 
     }
