ae48e39f928d7d8eeed09e3f7f0fe37366815deb
andrewreardigan <drewreardigan@gmail.com>
2023-07-29 19:52:08 -0400

    credit check

---
+++ src/main/java/methods/InstantCreditCheck.java
@@ -0,0 +1,47 @@
+package methods;
+
+import java.util.Scanner;
+
+public class InstantCreditCheck {
+
+    static int requiredSalary = 25000;
+    static int requiredCreditScore = 700;
+    static Scanner scanner = new Scanner(System.in);
+
+
+
+    public static void main(String[] args) {
+        double salary  = getSalary();
+        int creditScore = getCreditScore();
+        scanner.close();
+
+        boolean qualified = isUserQualified(salary,creditScore);
+        notifyUser(qualified);
+
+    }
+
+    private static void notifyUser(boolean qualified) {
+        if(qualified){
+            System.out.println("You're approved!");
+        }else System.out.println("DENIED!");
+    }
+
+    public static boolean isUserQualified(double salary, int creditScore) {
+        if(salary >= requiredSalary && creditScore >= requiredCreditScore){
+            return true;
+        } else return false;
+    }
+
+    public static double getSalary(){
+        System.out.println("Enter your salary");
+        double salary = scanner.nextDouble();
+        return salary;
+    }
+
+    public static int getCreditScore(){
+        System.out.println("Enter credit score..");
+        int creditScore = scanner.nextInt();
+        return creditScore;
+
+    };
+}
